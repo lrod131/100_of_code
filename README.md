97dddcbceb86331d31d87b3998e11cdee8435ea5
Luis <alexrod0890@gmail.com>
2023-04-02 01:02:57 -0300

    dia 1 parcial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 100_of_code
+
+En este repositorio guardaré mi progreso en el curo de 100 Days of code de Angela Yu.
---
+++ dia 1/ejercicio 2.py
@@ -0,0 +1,6 @@
+# strings new line
+print("hello World \nHello World \nHello World")
+
+# strings concatenation
+print("Hello" + " " + "Angela")
+
