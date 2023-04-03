47b9bf654a126d44efd40f353aad11e009ad541b
Luis <alexrod0890@gmail.com>
2023-04-02 22:26:36 -0300

    dia 1 completo

--- dia 1/ejercicio 2.py
+++
@@ -1,6 +0,0 @@
-# strings new line
-print("hello World \nHello World \nHello World")
-
-# strings concatenation
-print("Hello" + " " + "Angela")
-
---
+++ dia 1/proyecto_dia1.py
@@ -0,0 +1,5 @@
+print('Welcome to the Band Name Generator.')
+city = input("What's name of the city you grew up in? \n")
+pet_name = input("Tell me a pet's name: \n")
+
+print(f"Your band's name could be "+ city + " " + pet_name)
